c9543b6e8c0d3ec6465394f39433df894b903339
SHJ-Veronica <xiahluv91@gmail.com>
2014-02-06 14:36:36 +0530

    Erase All.

--- Team/movie-trailer/src/main/java/com/samsung/trailer/util/TrailerConstants.java
+++
@@ -1,5 +0,0 @@
-package com.samsung.trailer.util;
-
-public final class TrailerConstants {
-	public static final String TRAILER_COLLECTION = "trailer";
-}
--- movie-trailer/src/main/java/com/samsung/trailer/dao/TrailerDAO.java
+++
@@ -1,9 +0,0 @@
-package com.samsung.trailer.dao;
-
-import com.samsung.trailer.dao.common.BaseDAO;
-import com.samsung.trailer.vo.Trailer;
-
-
-public interface TrailerDAO extends BaseDAO<Trailer> {
-	
-}
